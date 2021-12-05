36e5c086b0b36f7d584c4e5293e16dc257a6d3c2
Ming Yong <mingyong.dev@gmail.com>
2021-12-05 02:05:45 -0500

    Clean up api file

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -2,39 +2,41 @@ import { Router, Request, Response } from 'express';
 import fs from 'fs';
 import axios from 'axios';
 import { AppError } from '../typings/AppError';
-import { User, Repo } from '../typings/Repo';
+import { Repo } from '../typings/Repo';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-  res.status(200);  
+  res.status(200);
 
   let result: Repo[] | AppError;
-  
+
   try {
-    const GITHUB_URI = "https://api.github.com/users/silverorange/repos";
-    const LOCAL_FILE_PATH = "./data/repos.json";
-    let allRepos:Repo[];
+    const GITHUB_URI = 'https://api.github.com/users/silverorange/repos';
+    const LOCAL_FILE_PATH = './data/repos.json';
+    let allRepos: Repo[];
 
     //Get github repos
-    const response = await axios.get(GITHUB_URI);    
+    const response = await axios.get(GITHUB_URI);
     allRepos = [...response.data];
 
     //Get local repos
-    let rawFileData = fs.readFileSync(LOCAL_FILE_PATH, 'utf8');
-    let localRepos = JSON.parse(rawFileData);
+    const rawFileData = fs.readFileSync(LOCAL_FILE_PATH, 'utf8');
+    const localRepos = JSON.parse(rawFileData);
     allRepos.push(...localRepos);
 
     //Filter repos
-    allRepos = allRepos.filter((repo:any) => !repo.fork);
+    allRepos = allRepos.filter((repo: any) => !repo.fork);
 
     result = allRepos;
-
   } catch (error) {
-    const ERROR:AppError = {name: "custom error", status: 500, message: "Something went wrong!"};
+    const ERROR: AppError = {
+      name: 'custom error',
+      status: 500,
+      message: 'Something went wrong!',
+    };
     result = ERROR;
-    console.error(error);
   }
 
   res.json(result);
