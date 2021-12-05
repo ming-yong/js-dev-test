459bf5d544a633419ad261261853b2a678bda0ac
Ming Yong <mingyong.dev@gmail.com>
2021-12-05 02:58:39 -0500

    Removed unuse code for App component -- end of 4 hours

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-// import logo from './logo.svg';
-
 import './App.css';
 
 const fillerRepo = {
@@ -54,20 +52,6 @@ export function App() {
       ) : (
         <p>{displayData}</p>
       )}
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
