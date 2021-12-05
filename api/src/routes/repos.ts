6ea4025640431a6afd1144a1d12b82e8ca7d9c36
Ming Yong <mingyong.dev@gmail.com>
2021-12-05 00:39:23 -0500

    Initial build for repos route - return error if any of the sources throws error - return filtered array if fetched

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,41 @@
 import { Router, Request, Response } from 'express';
+import fs from 'fs';
+import axios from 'axios';
+import { AppError } from '../typings/AppError';
+import { User, Repo } from '../typings/Repo';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
+  res.status(200);  
 
-  res.status(200);
+  let result: Repo[] | AppError;
+  
+  try {
+    const GITHUB_URI = "https://api.github.com/users/silverorange/repos";
+    const LOCAL_FILE_PATH = "./data/repos.json";
+    let allRepos:Repo[];
 
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+    //Get github repos
+    const response = await axios.get(GITHUB_URI);    
+    allRepos = [...response.data];
+
+    //Get local repos
+    let rawFileData = fs.readFileSync(LOCAL_FILE_PATH, 'utf8');
+    let localRepos = JSON.parse(rawFileData);
+    allRepos.push(...localRepos);
+
+    //Filter repos
+    allRepos = allRepos.filter((repo:any) => !repo.fork);
+
+    result = allRepos;
+
+  } catch (error) {
+    const ERROR:AppError = {name: "custom error", status: 500, message: "Something went wrong!"};
+    result = ERROR;
+    console.error(error);
+  }
+
+  res.json(result);
 });
