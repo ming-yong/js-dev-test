7d5788b4b4b268230e2f7a7eb934e63b2f87377d
Ming Yong <mingyong.dev@gmail.com>
2021-12-05 02:56:11 -0500

    Fetch data on front-end and display data

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+// import logo from './logo.svg';
 
 import './App.css';
 
+const fillerRepo = {
+  name: 'hi',
+  description: 'filler',
+  language: 'english',
+  forks: 0,
+};
+
 export function App() {
+  const [displayData, setDisplayData] = useState([fillerRepo]);
+  const [success, setSuccess] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/repos')
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === 500) {
+          setSuccess(false);
+          setDisplayData(data.message);
+          return;
+        }
+        setDisplayData(data);
+        setSuccess(true);
+      })
+      .catch((error) => {
+        setDisplayData(error);
+        setSuccess(false);
+      });
+  }, []);
+
+  console.log(displayData);
+
   return (
     <div className="App">
-      <header className="App-header">
+      {success && Array.isArray(displayData) ? (
+        <ul>
+          {displayData.map((repo, index) => (
+            <li key={index}>
+              {repo.name}
+              <ul>
+                <li>
+                  Description: {repo.description ? repo.description : 'none'}
+                </li>
+                <li>Language: {repo.language}</li>
+                <li>Forks: {repo.forks}</li>
+              </ul>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>{displayData}</p>
+      )}
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -19,7 +67,7 @@ export function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
